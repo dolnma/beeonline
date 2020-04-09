f4e08f1d74810c66863c822ce53215c5e70ec5ce
Marek Dolníček <marek.dolnicek@coolcredit.cz>
2020-04-09 16:40:45 +0200

    fixed some small bugs and recaptcha

--- src/mail.php
+++ src/mail.php
@@ -4,59 +4,97 @@ $errorMSG = "";
 
 // NAME
 if (empty($_POST["name"])) {
-    $errorMSG = "Name is required";
+    $errorMSG = "Jméno a příjmení je povinné ";
 } else {
-    $name = $_POST["name"];
+    $name = html_entity_decode($_POST["name"]);
 }
-
+// PHONE
 if (empty($_POST["phone"])) {
-    $errorMSG = "Phone is required ";
+    $errorMSG = "Telefon je povinný ";
 } else {
     $phone = $_POST["phone"];
 }
 
 // EMAIL
 if (empty($_POST["email"])) {
-    $errorMSG .= "email is requiredy";
+    $errorMSG .= "Email je povinný ";
 } else {
     $email = $_POST["email"];
 }
 
 // MESSAGE
 if (empty($_POST["message"])) {
-    $errorMSG .= "Message is requiredy";
+    $errorMSG .= "Zpráva je povinná ";
 } else {
     $message = $_POST["message"];
 }
 
-//Add your email here
-$EmailTo = "marek.dolnicek@gmail.com";
-$Subject = "New message from website Beeonline from $name";
+// Recaptcha
+if (empty($_POST["g-recaptcha-response"])) {
+    $errorMSG .= "Špatně zadaná reCAPTCHA";
+} else {
+    $captcha = $_POST["g-recaptcha-response"];
+}
+
+// Force PHP to use the UTF-8 charset
+//header('Content-Type: text/html; charset=utf-8');
+
+// Define the sender
+$sender = $email;
+// or:
+// $sender = '=?UTF-8?B?' . base64_encode('John Döe') . '?= <sender@domain.com>';
+
+// Define the recipient
+$recipient = 'pavel.fric@beeonline.cz';
+// or:
+// $recipient = '=?UTF-8?B?' . base64_encode('Margret Müller') . '?= <recipient@domain.com>';
+
+// Add custom headers
+$headers = 'Content-Type: text/plain; charset=utf-8' . "\r\n";
+$headers .= 'Content-Transfer-Encoding: base64' . "\r\n";
+$headers .= 'From: ' . $sender;
+
+// Define and Base64 encode the subject line
+$subject_text = 'Nová zpráva z webu BeeOnline.cz od ' . $name . '';
+$subject = '=?UTF-8?B?' . base64_encode($subject_text) . '?=';
 
 // prepare email body text
 $Body = "";
-$Body .= "Name: ";
+$Body .= "Jméno a příjmení: ";
 $Body .= $name;
 $Body .= "\n";
-$Body .= "Phone: ";
+$Body .= "Telefon: ";
 $Body .= $phone;
 $Body .= "\n";
 $Body .= "Email: ";
 $Body .= $email;
 $Body .= "\n";
-$Body .= "Message: ";
+$Body .= "Zpráva: ";
 $Body .= $message;
 $Body .= "\n";
 
-// send email
-$success = mail($EmailTo, $Subject, $Body, "From:".$email);
+// Define and Base64 the email body text
+$message = base64_encode($Body);
+
+$secretKey = "6LeMwOcUAAAAAFvQt84IeTzdfdRxVDhR3qe3DYSS";
+$ip = $_SERVER['REMOTE_ADDR'];
+// post request to server
+$url = 'https://www.google.com/recaptcha/api/siteverify?secret=' . urlencode($secretKey) .  '&response=' . urlencode($captcha);
+$response = file_get_contents($url);
+$responseKeys = json_decode($response,true);
+// should return JSON with success as true
+if($responseKeys["success"]) {
+    $success = mail($recipient, $subject, $message, $headers);
+} else {
+    $errorMSG .= "Špatně zadaná reCAPTCHA";
+}
 
 // redirect to success page
-if ($success && $errorMSG == ""){
-    echo "success";
-}else{
-    if($errorMSG == ""){
-        echo "Error sending message";
+if ($success && $errorMSG == "") {
+    echo json_encode('success');
+} else {
+    if ($errorMSG == "") {
+        echo "Odeslání nebylo úspěšné";
     } else {
         echo $errorMSG;
     }
--- src/scripts/form.js
+++ src/scripts/form.js
@@ -23,8 +23,9 @@ function submitForm(divEl){
     let phone = $("#phone").val()
     let email = $("#email").val()
     let message = $("#message").val()
+    let recaptcha = grecaptcha.getResponse()
 
-    let dataString = "name=" + name + "&phone=" + phone + "&email=" + email + "&message=" + message
+    let dataString = "name=" + name + "&phone=" + phone + "&email=" + email + "&message=" + message + "&g-recaptcha-response=" + recaptcha
 
     $.ajax({
         type: "POST",
@@ -33,30 +34,30 @@ function submitForm(divEl){
         beforeSend: function() {
             $(divEl).find('.form__wrapper').html('<div class="loader">Loading...</div>')
         },
-        success : function(text){
-            if (text == "success"){
-                formSuccess(divEl);
+        success : function(response){
+            let result = JSON.parse(response)
+            if (result === 'success'){
+                formSuccess(divEl)
             } else {
-                formError(divEl);
-                console.log('Error sending form')
+                formError(divEl)
             }
         }
-    });
+    })
 }
 
 function formSuccess(divEl){
-    $(divEl)[0].reset();
+    $(divEl)[0].reset()
     submitMSG(divEl)
 }
 
 function formError(divEl){
     $(divEl).removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-        $(this).removeClass();
+        $(this).removeClass()
     });
 }
 
 let msgClasses
 
 function submitMSG(divEl){
-    $('.form__wrapper').html('<div class="done"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 130.2 130.2"><circle class="path circle" fill="none" stroke="#73AF55" stroke-width="6" stroke-miterlimit="10" cx="65.1" cy="65.1" r="62.1"/> <polyline class="path check" fill="none" stroke="#73AF55" stroke-width="6" stroke-linecap="round" stroke-miterlimit="10" points="100.2,40.2 51.5,88.8 29.8,67.5 "/> </svg><p class="success">Thanks for your message. We will contact you!</p></div>');
+    $('.form__wrapper').html('<div class="done"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 130.2 130.2"><circle class="path circle" fill="none" stroke="#73AF55" stroke-width="6" stroke-miterlimit="10" cx="65.1" cy="65.1" r="62.1"/> <polyline class="path check" fill="none" stroke="#73AF55" stroke-width="6" stroke-linecap="round" stroke-miterlimit="10" points="100.2,40.2 51.5,88.8 29.8,67.5 "/> </svg><p class="success">Děkujeme. Budeme Vás kontaktovat!</p></div>');
 }
