df006d0ac5e4f48c56ae87794e7b7fa884c8017a
Marek Dolníček <marek.dolnicek@coolcredit.cz>
2020-04-14 21:31:33 +0200

    fixed mail

--- src/scripts/form.js
+++ src/scripts/form.js
@@ -17,7 +17,7 @@ export const formInit = (el) => {
     })
 }
 
-function submitForm(divEl){
+function submitForm(divEl) {
     // Initiate Variables With Form Content
     let name = $("#name").val()
     let phone = $("#phone").val()
@@ -53,11 +53,11 @@ function formSuccess(divEl){
 function formError(divEl){
     $(divEl).removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
         $(this).removeClass()
-    });
+    })
 }
 
 let msgClasses
 
 function submitMSG(divEl){
-    $('.form__wrapper').html('<div class="done"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 130.2 130.2"><circle class="path circle" fill="none" stroke="#73AF55" stroke-width="6" stroke-miterlimit="10" cx="65.1" cy="65.1" r="62.1"/> <polyline class="path check" fill="none" stroke="#73AF55" stroke-width="6" stroke-linecap="round" stroke-miterlimit="10" points="100.2,40.2 51.5,88.8 29.8,67.5 "/> </svg><p class="success">Děkujeme. Budeme Vás kontaktovat!</p></div>');
+    $('.form__wrapper').html('<div class="done"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 130.2 130.2"><circle class="path circle" fill="none" stroke="#73AF55" stroke-width="6" stroke-miterlimit="10" cx="65.1" cy="65.1" r="62.1"/> <polyline class="path check" fill="none" stroke="#73AF55" stroke-width="6" stroke-linecap="round" stroke-miterlimit="10" points="100.2,40.2 51.5,88.8 29.8,67.5 "/> </svg><p class="success">Děkujeme. Budeme Vás kontaktovat!</p></div>')
 }
